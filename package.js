696b039430f3ea14f0a358a96c14b7fb71fc97c6
Michal Wanat <michalwanat@gmail.com>
2016-01-23 13:52:12 +0100

    less

--- package.js
+++ package.js
@@ -7,15 +7,17 @@ Package.describe({
     git: 'https://github.com/vazco/meteor-universe-autoform-select.git'
 });
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
     api.versionsFrom('1.2.1');
 
-    if(!api.addAssets) {
-        api.addAssets = function(files, platform){
+    if (!api.addAssets) {
+        api.addAssets = function (files, platform) {
             api.addFiles(files, platform, {isAsset: true})
         };
     }
 
+    api.use('ecmascript');
+    api.use('less');
     api.use('templating');
     api.use('aldeed:autoform@5.8.0');
 
@@ -26,8 +28,8 @@ Package.onUse(function(api) {
     api.addFiles([
         'universe-autoform-select.html',
         'universe-autoform-select.js',
-        'stylesheets/selectize.default.css',
-        'stylesheets/universe-autoform-select.css'
+        'stylesheets/selectize.default.less',
+        'stylesheets/universe-autoform-select.less'
     ], 'client');
 
     api.addAssets('img/loading.gif', 'client');
