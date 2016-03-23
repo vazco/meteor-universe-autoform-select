bf10b044b1a5e9dbaab2f871d914810c4aa3e1f7
Michal Wanat <michalwanat@gmail.com>
2016-03-23 10:04:12 +0100

    cleaning

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 Package.describe({
     name: 'vazco:universe-autoform-select',
     summary: 'Custom "afUniverseSelect" input type for AutoForm, with the appearance as selectize',
-    version: '0.3.4',
+    version: '0.3.5',
     git: 'https://github.com/vazco/meteor-universe-autoform-select.git'
 });
 
@@ -19,16 +19,12 @@ Package.onUse(function (api) {
     api.use('ecmascript');
     api.use('templating');
     api.use('aldeed:autoform@5.8.1');
-    api.use('vazco:universe-selectize@0.1.6');
+    api.use('vazco:universe-selectize@0.1.11');
 
-    api.use(['underscore', 'reactive-var'], 'client');
-
-    api.addFiles('vendor/speakingurl.min.js', 'client');
+    api.use(['underscore'], 'client');
 
     api.addFiles([
         'universe-autoform-select.html',
         'universe-autoform-select.js'
     ], 'client');
-
-    api.addAssets('img/loading.gif', 'client');
 });
